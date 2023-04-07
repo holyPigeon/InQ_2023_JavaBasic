7753c7a9970f3276aa3c885b3891acd4455e92b3
hupa7 <hupa2525@gmail.com>
2023-04-07 17:03:52 +0900

    [2ì[2주차 경성현 과제제출

---
+++ src/성현/Week2_2753.java
@@ -0,0 +1,14 @@
+package 성현;
+import java.util.*;
+public class Week2_2753 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        int year=s.nextInt();
+        if((year%4==0&&year%100!=0)||year%400==0){
+            System.out.println("1");
+
+        }
+        else
+            System.out.println("0");
+    }
+}
