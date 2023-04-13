051d0cb5c9e6b4f9b035ebc5b9b84b678ae0e0aa
JeongMin <128018186+JeongMin-2@users.noreply.github.com>
2023-04-14 00:19:41 +0900

    [2주차] 이정민 과제 제출 (#23)
    * [2주차] 이정민 과제 제출
    
    * Delete .gitignore
    
    * Delete misc.xml
    
    * Delete modules.xml
    
    * Delete vcs.xml
    
    * delete iml

---
+++ src/정민/Week2_2753.java
@@ -0,0 +1,22 @@
+package 정민;
+import java.util.Scanner;
+public class Week2_2753 {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int year = s.nextInt();
+
+        if(year % 4 == 0)
+        {
+            if(year % 100 != 0 || year % 400 == 0) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        }
+        else System.out.println("0");
+
+    }
+
+}
